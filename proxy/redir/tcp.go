63308472ad5b66d1151cecf2468b8f566de72a04
Dreamacro <305009791@qq.com>
2018-08-12 04:00:34 +0800

    New: redir proxy

---
+++ proxy/redir/tcp.go
@@ -0,0 +1,62 @@
+package redir
+
+import (
+	"net"
+
+	"github.com/Dreamacro/clash/adapters/local"
+	C "github.com/Dreamacro/clash/constant"
+	"github.com/Dreamacro/clash/tunnel"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	tun = tunnel.Instance()
+)
+
+func NewRedirProxy(addr string) (*C.ProxySignal, error) {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	done := make(chan struct{})
+	closed := make(chan struct{})
+	signal := &C.ProxySignal{
+		Done:   done,
+		Closed: closed,
+	}
+
+	go func() {
+		log.Infof("Redir proxy listening at: %s", addr)
+		for {
+			c, err := l.Accept()
+			if err != nil {
+				if _, open := <-done; !open {
+					break
+				}
+				continue
+			}
+			go handleRedir(c)
+		}
+	}()
+
+	go func() {
+		<-done
+		close(done)
+		l.Close()
+		closed <- struct{}{}
+	}()
+
+	return signal, nil
+}
+
+func handleRedir(conn net.Conn) {
+	target, err := parserPacket(conn)
+	if err != nil {
+		conn.Close()
+		return
+	}
+	conn.(*net.TCPConn).SetKeepAlive(true)
+	tun.Add(adapters.NewSocks(target, conn))
+}
