af13acc17151159c839c1f28900185990f435d5e
Dreamacro <305009791@qq.com>
2018-09-03 23:52:16 +0800

    Clean: unused code

--- adapters/local/util.go
+++ adapters/local/util.go
@@ -1,8 +1,6 @@
 package adapters
 
 import (
-	"bufio"
-	"bytes"
 	"net"
 	"net/http"
 	"strconv"
@@ -70,77 +68,4 @@ func parseHTTPAddr(request *http.Request) *C.Addr {
 		IP:       resolveIP,
 		Port:     port,
 	}
-}
-
-// ParserHTTPHostHeader returns the HTTP Host header from br without
-// consuming any of its bytes. It returns "" if it can't find one.
-func ParserHTTPHostHeader(br *bufio.Reader) (method, host string) {
-	// br := bufio.NewReader(bytes.NewReader(data))
-	const maxPeek = 4 << 10
-	peekSize := 0
-	for {
-		peekSize++
-		if peekSize > maxPeek {
-			b, _ := br.Peek(br.Buffered())
-			return method, httpHostHeaderFromBytes(b)
-		}
-		b, err := br.Peek(peekSize)
-		if n := br.Buffered(); n > peekSize {
-			b, _ = br.Peek(n)
-			peekSize = n
-		}
-		if len(b) > 0 {
-			if b[0] < 'A' || b[0] > 'Z' {
-				// Doesn't look like an HTTP verb
-				// (GET, POST, etc).
-				return
-			}
-			if bytes.Index(b, crlfcrlf) != -1 || bytes.Index(b, lflf) != -1 {
-				println(string(b))
-				req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(b)))
-				if err != nil {
-					return
-				}
-				if len(req.Header["Host"]) > 1 {
-					// TODO(bradfitz): what does
-					// ReadRequest do if there are
-					// multiple Host headers?
-					return
-				}
-				println(req.Host)
-				return req.Method, req.Host
-			}
-		}
-		if err != nil {
-			return method, httpHostHeaderFromBytes(b)
-		}
-	}
-}
-
-var (
-	lfHostColon = []byte("\nHost:")
-	lfhostColon = []byte("\nhost:")
-	crlf        = []byte("\r\n")
-	lf          = []byte("\n")
-	crlfcrlf    = []byte("\r\n\r\n")
-	lflf        = []byte("\n\n")
-)
-
-func httpHostHeaderFromBytes(b []byte) string {
-	if i := bytes.Index(b, lfHostColon); i != -1 {
-		return string(bytes.TrimSpace(untilEOL(b[i+len(lfHostColon):])))
-	}
-	if i := bytes.Index(b, lfhostColon); i != -1 {
-		return string(bytes.TrimSpace(untilEOL(b[i+len(lfhostColon):])))
-	}
-	return ""
-}
-
-// untilEOL returns v, truncated before the first '\n' byte, if any.
-// The returned slice may include a '\r' at the end.
-func untilEOL(v []byte) []byte {
-	if i := bytes.IndexByte(v, '\n'); i != -1 {
-		return v[:i]
-	}
-	return v
 }
