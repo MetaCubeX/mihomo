537b672fcf9917c51003291bc73145d6713520f2
Dreamacro <8615343+Dreamacro@users.noreply.github.com>
2021-10-04 19:20:11 +0800

    Change: use bbolt as cache db

--- constant/path.go
+++ constant/path.go
@@ -55,6 +55,10 @@ func (p *path) MMDB() string {
 	return P.Join(p.homeDir, "Country.mmdb")
 }
 
-func (p *path) Cache() string {
+func (p *path) OldCache() string {
 	return P.Join(p.homeDir, ".cache")
+}
+
+func (p *path) Cache() string {
+	return P.Join(p.homeDir, "cache.db")
 }
