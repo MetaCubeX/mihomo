MetaCubeX/mihomo

commit d40e5e4fe6c11311ee1de82779a985c3ca47d03a

Fix: codeql alerts

adapter/outbound/snell.go
@@ -52,7 +52,7 @@ func streamConn(c net.Conn, option streamOption) *snell.Snell {
 // StreamConn implements C.ProxyAdapter
 func (s *Snell) StreamConn(c net.Conn, metadata *C.Metadata) (net.Conn, error) {
 	c = streamConn(c, streamOption{s.psk, s.version, s.addr, s.obfsOption})
-	port, _ := strconv.Atoi(metadata.DstPort)
+	port, _ := strconv.ParseInt(metadata.DstPort, 10, 16)
 	err := snell.WriteHeader(c, metadata.String(), uint(port), s.version)
 	return c, err
 }
@@ -65,7 +65,7 @@ func (s *Snell) DialContext(ctx context.Context, metadata *C.Metadata, opts ...d
 			return nil, err
 		}
 
-		port, _ := strconv.Atoi(metadata.DstPort)
+		port, _ := strconv.ParseUint(metadata.DstPort, 10, 16)
 		if err = snell.WriteHeader(c, metadata.String(), uint(port), s.version); err != nil {
 			c.Close()
 			return nil, err

component/dialer/bind_others.go
@@ -62,7 +62,7 @@ func bindIfaceToDialer(ifaceName string, dialer *net.Dialer, network string, des
 	if dialer.LocalAddr != nil {
 		_, port, err := net.SplitHostPort(dialer.LocalAddr.String())
 		if err == nil {
-			local, _ = strconv.Atoi(port)
+			local, _ = strconv.ParseInt(port, 10, 16)
 		}
 	}
 
@@ -82,7 +82,7 @@ func bindIfaceToListenConfig(ifaceName string, _ *net.ListenConfig, network, add
 		port = "0"
 	}
 
-	local, _ := strconv.Atoi(port)
+	local, _ := strconv.ParseInt(port, 10, 16)
 
 	addr, err := lookupLocalAddr(ifaceName, network, nil, local)
 	if err != nil {

constant/metadata.go
@@ -107,10 +107,10 @@ func (m *Metadata) UDPAddr() *net.UDPAddr {
 	if m.NetWork != UDP || m.DstIP == nil {
 		return nil
 	}
-	port, _ := strconv.Atoi(m.DstPort)
+	port, _ := strconv.ParseInt(m.DstPort, 10, 16)
 	return &net.UDPAddr{
 		IP:   m.DstIP,
-		Port: port,
+		Port: int(port),
 	}
 }
 

adapter/outbound/util.go
@@ -21,7 +21,7 @@ func tcpKeepAlive(c net.Conn) {
 func serializesSocksAddr(metadata *C.Metadata) []byte {
 	var buf [][]byte
 	aType := uint8(metadata.AddrType)
-	p, _ := strconv.Atoi(metadata.DstPort)
+	p, _ := strconv.ParseUint(metadata.DstPort, 10, 16)
 	port := []byte{uint8(p >> 8), uint8(p & 0xff)}
 	switch metadata.AddrType {
 	case socks5.AtypDomainName:

adapter/outbound/vmess.go
@@ -353,7 +353,7 @@ func parseVmessAddr(metadata *C.Metadata) *vmess.DstAddr {
 		copy(addr[1:], []byte(metadata.Host))
 	}
 
-	port, _ := strconv.Atoi(metadata.DstPort)
+	port, _ := strconv.ParseUint(metadata.DstPort, 10, 16)
 	return &vmess.DstAddr{
 		UDP:      metadata.NetWork == C.UDP,
 		AddrType: addrType,

rule/port.go
@@ -39,7 +39,7 @@ func (p *Port) ShouldResolveIP() bool {
 }
 
 func NewPort(port string, adapter string, isSource bool) (*Port, error) {
-	_, err := strconv.Atoi(port)
+	_, err := strconv.ParseUint(port, 10, 16)
 	if err != nil {
 		return nil, errPayload
 	}

commit 8679968ab0a3112cfe4368118626e879609bfe67

Fix: multiple port string parsing overflow (#1868)
Ports in TCP and UDP should be parsed as an unsigned integer,
otherwise ports > 32767 get truncated to 32767. As this is
the case with Metadata.UDPAddr(), this fundamentally breaks
UDP connections where demand for high port numbers is high.

This commit fixes all known cases where ParseInt is used for ports,
and has been verified to fix Discord voice connections on port
50001~50004.

Fixes: d40e5e4fe6c11311ee1de82779a985c3ca47d03a

Co-authored-by: Hamster Tian <haotia@gmail.com>

adapter/outbound/snell.go
@@ -52,7 +52,7 @@ func streamConn(c net.Conn, option streamOption) *snell.Snell {
 // StreamConn implements C.ProxyAdapter
 func (s *Snell) StreamConn(c net.Conn, metadata *C.Metadata) (net.Conn, error) {
 	c = streamConn(c, streamOption{s.psk, s.version, s.addr, s.obfsOption})
-	port, _ := strconv.ParseInt(metadata.DstPort, 10, 16)
+	port, _ := strconv.ParseUint(metadata.DstPort, 10, 16)
 	err := snell.WriteHeader(c, metadata.String(), uint(port), s.version)
 	return c, err
 }

component/dialer/bind_others.go
@@ -58,11 +58,11 @@ func bindIfaceToDialer(ifaceName string, dialer *net.Dialer, network string, des
 		return nil
 	}
 
-	local := int64(0)
+	local := uint64(0)
 	if dialer.LocalAddr != nil {
 		_, port, err := net.SplitHostPort(dialer.LocalAddr.String())
 		if err == nil {
-			local, _ = strconv.ParseInt(port, 10, 16)
+			local, _ = strconv.ParseUint(port, 10, 16)
 		}
 	}
 
@@ -82,7 +82,7 @@ func bindIfaceToListenConfig(ifaceName string, _ *net.ListenConfig, network, add
 		port = "0"
 	}
 
-	local, _ := strconv.ParseInt(port, 10, 16)
+	local, _ := strconv.ParseUint(port, 10, 16)
 
 	addr, err := lookupLocalAddr(ifaceName, network, nil, int(local))
 	if err != nil {

constant/metadata.go
@@ -107,7 +107,7 @@ func (m *Metadata) UDPAddr() *net.UDPAddr {
 	if m.NetWork != UDP || m.DstIP == nil {
 		return nil
 	}
-	port, _ := strconv.ParseInt(m.DstPort, 10, 16)
+	port, _ := strconv.ParseUint(m.DstPort, 10, 16)
 	return &net.UDPAddr{
 		IP:   m.DstIP,
 		Port: int(port),