9e9c3c810faa3a35dec447d08ca22f5e4a230867
lelemka0 <20322043+lelemka0@users.noreply.github.com>
2022-05-06 11:43:53 +0800

    fixed: make log api unblocked

--- log/log.go
+++ log/log.go
@@ -10,8 +10,8 @@ import (
 )
 
 var (
-	logCh  = make(chan *Event)
-	source = observable.NewObservable[*Event](logCh)
+	logCh  = make(chan Event)
+	source = observable.NewObservable[Event](logCh)
 	level  = INFO
 )
 
@@ -57,12 +57,12 @@ func Fatalln(format string, v ...any) {
 	log.Fatalf(format, v...)
 }
 
-func Subscribe() observable.Subscription[*Event] {
+func Subscribe() observable.Subscription[Event] {
 	sub, _ := source.Subscribe()
 	return sub
 }
 
-func UnSubscribe(sub observable.Subscription[*Event]) {
+func UnSubscribe(sub observable.Subscription[Event]) {
 	source.UnSubscribe(sub)
 }
 
@@ -74,7 +74,7 @@ func SetLevel(newLevel LogLevel) {
 	level = newLevel
 }
 
-func print(data *Event) {
+func print(data Event) {
 	if data.LogLevel < level {
 		return
 	}
@@ -91,15 +91,9 @@ func print(data *Event) {
 	}
 }
 
-func newLog(logLevel LogLevel, format string, v ...any) *Event {
-	return &Event{
+func newLog(logLevel LogLevel, format string, v ...any) Event {
+	return Event{
 		LogLevel: logLevel,
 		Payload:  fmt.Sprintf(format, v...),
 	}
-}
-
-func PrintLog(logLevel LogLevel, format string, v ...interface{}) {
-	event := newLog(logLevel, format, v...)
-	logCh <- event
-	print(event)
 }
