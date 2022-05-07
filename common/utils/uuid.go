4ab91520bd031828d37bc3b2e2c53dcd2abca06d
Skyxim <noreply@skyxim.com>
2022-05-07 12:35:14 +0800

    refactor: reuse uuid namespace

--- common/utils/uuid.go
+++ common/utils/uuid.go
@@ -4,11 +4,12 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+var uuidNamespace, _ = uuid.FromString("00000000-0000-0000-0000-000000000000")
+
 // UUIDMap https://github.com/XTLS/Xray-core/issues/158#issue-783294090
 func UUIDMap(str string) (uuid.UUID, error) {
 	u, err := uuid.FromString(str)
 	if err != nil {
-		uuidNamespace, err := uuid.FromString("00000000-0000-0000-0000-000000000000")
 		if err != nil {
 			return uuid.UUID{}, err
 		}
