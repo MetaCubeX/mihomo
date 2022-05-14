63917aa0202f216f27931d820e665347c658044c
Skyxim <skyxim@noreply.com>
2022-05-14 23:45:10 +0800

    fix: uuid-map return failed error

--- common/utils/uuid.go
+++ common/utils/uuid.go
@@ -10,7 +10,7 @@ var uuidNamespace, _ = uuid.FromString("00000000-0000-0000-0000-000000000000")
 func UUIDMap(str string) (uuid.UUID, error) {
 	u, err := uuid.FromString(str)
 	if err != nil {
-		return uuid.NewV5(uuidNamespace, str), err
+		return uuid.NewV5(uuidNamespace, str), nil
 	}
 	return u, nil
 }
