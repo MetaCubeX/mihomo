1acc6759e9ac599ff15e48a1ccd5c1ea42dda31d
世界 <i@sekai.icu>
2022-06-06 19:52:02 +0800

    feat: add support for shadowsocks 2022 ciphers

--- common/pool/alloc.go
+++ common/pool/alloc.go
@@ -8,7 +8,7 @@ import (
 	"sync"
 )
 
-var defaultAllocator = NewAllocator()
+var DefaultAllocator = NewAllocator()
 
 // Allocator for incoming frames, optimized to prevent overwriting after zeroing
 type Allocator struct {
