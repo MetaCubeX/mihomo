fdd2f49241dbdf3033ba6bb069c543a821a18d8d
Dreamacro <8615343+dreamacro@users.noreply.github.com>
2022-07-05 21:09:29 +0800

    Fix: fakeip udp should not replace with another ip

--- tunnel/connection.go
+++ tunnel/connection.go
@@ -3,11 +3,11 @@ package tunnel
 import (
 	"errors"
 	"net"
+	"net/netip"
 	"time"
 
 	N "github.com/Dreamacro/clash/common/net"
 	"github.com/Dreamacro/clash/common/pool"
-	"github.com/Dreamacro/clash/component/resolver"
 	C "github.com/Dreamacro/clash/constant"
 )
 
@@ -15,15 +15,6 @@ func handleUDPToRemote(packet C.UDPPacket, pc net.PacketConn, metadata *C.Metada
 	pc = unwrapPacket(pc)
 
 	defer packet.Drop()
-
-	// local resolve UDP dns
-	if !metadata.Resolved() {
-		ip, err := resolver.ResolveIP(metadata.Host)
-		if err != nil {
-			return err
-		}
-		metadata.DstIP = ip
-	}
 
 	addr := metadata.UDPAddr()
 	if addr == nil {
@@ -34,31 +25,36 @@ func handleUDPToRemote(packet C.UDPPacket, pc net.PacketConn, metadata *C.Metada
 		return err
 	}
 	// reset timeout
-	pc.SetReadDeadline(time.Now().Add(udpTimeout))
+	_ = pc.SetReadDeadline(time.Now().Add(udpTimeout))
 
 	return nil
 }
 
-func handleUDPToLocal(packet C.UDPPacket, pc net.PacketConn, key string, fAddr net.Addr) {
-	pc = unwrapPacket(pc)
-
+func handleUDPToLocal(packet C.UDPPacket, pc net.PacketConn, key string, oAddr, fAddr netip.Addr) {
 	buf := pool.Get(pool.UDPBufferSize)
-	defer pool.Put(buf)
-	defer natTable.Delete(key)
-	defer pc.Close()
+	defer func() {
+		_ = pc.Close()
+		natTable.Delete(key)
+		_ = pool.Put(buf)
+	}()
 
 	for {
-		pc.SetReadDeadline(time.Now().Add(udpTimeout))
+		_ = pc.SetReadDeadline(time.Now().Add(udpTimeout))
 		n, from, err := pc.ReadFrom(buf)
 		if err != nil {
 			return
 		}
 
-		if fAddr != nil {
-			from = fAddr
+		fromUDPAddr := from.(*net.UDPAddr)
+		if fAddr.IsValid() {
+			fromAddr, _ := netip.AddrFromSlice(fromUDPAddr.IP)
+			fromAddr.Unmap()
+			if oAddr == fromAddr {
+				fromUDPAddr.IP = fAddr.AsSlice()
+			}
 		}
 
-		_, err = packet.WriteBack(buf[:n], from)
+		_, err = packet.WriteBack(buf[:n], fromUDPAddr)
 		if err != nil {
 			return
 		}
