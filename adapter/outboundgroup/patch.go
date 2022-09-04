c3846faf01a14df16045ddea58fa32e83ad70b25
Howard Cheung <mail@h-cheung.cf>
2022-09-04 14:55:52 +0800

    fix: proxy providers not filtered in proxy groups

--- adapter/outboundgroup/patch.go
+++ adapter/outboundgroup/patch.go
@@ -9,6 +9,7 @@ type ProxyGroup interface {
 	C.ProxyAdapter
 
 	Providers() []provider.ProxyProvider
+	Proxies() []C.Proxy
 	Now() string
 }
 
@@ -16,8 +17,16 @@ func (f *Fallback) Providers() []provider.ProxyProvider {
 	return f.providers
 }
 
+func (f *Fallback) Proxies() []C.Proxy {
+	return f.GetProxies(false)
+}
+
 func (lb *LoadBalance) Providers() []provider.ProxyProvider {
 	return lb.providers
+}
+
+func (lb *LoadBalance) Proxies() []C.Proxy {
+	return lb.GetProxies(false)
 }
 
 func (lb *LoadBalance) Now() string {
@@ -28,6 +37,10 @@ func (r *Relay) Providers() []provider.ProxyProvider {
 	return r.providers
 }
 
+func (r *Relay) Proxies() []C.Proxy {
+	return r.GetProxies(false)
+}
+
 func (r *Relay) Now() string {
 	return ""
 }
@@ -36,6 +49,14 @@ func (s *Selector) Providers() []provider.ProxyProvider {
 	return s.providers
 }
 
+func (s *Selector) Proxies() []C.Proxy {
+	return s.GetProxies(false)
+}
+
 func (u *URLTest) Providers() []provider.ProxyProvider {
 	return u.providers
+}
+
+func (u *URLTest) Proxies() []C.Proxy {
+	return u.GetProxies(false)
 }
