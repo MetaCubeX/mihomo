a7233f603620d0e31ef3651d8eb5c3501d892e57
Skyxim <noreply@skyxim.dev>
2023-04-28 16:55:35 +0000

    fix: wildcard matching problem

--- component/trie/domain_set.go
+++ component/trie/domain_set.go
@@ -104,8 +104,8 @@ func (ss *DomainSet) Has(key string) bool {
 							goto RESTART
 						}
 					}
-					for ; ; nextBmIdx++ {
-						if nextBmIdx-nextNodeId < len(ss.labels) && ss.labels[nextBmIdx-nextNodeId] == domainStepByte {
+					for ; nextBmIdx-nextNodeId < len(ss.labels); nextBmIdx++ {
+						if ss.labels[nextBmIdx-nextNodeId] == domainStepByte {
 							bmIdx = nextBmIdx
 							nodeId = nextNodeId
 							i = j
