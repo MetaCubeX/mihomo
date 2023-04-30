1be62bca87c3cdad50da038fdfd8abc96821efb5
sleshep <9625413+sleshep@users.noreply.github.com>
2023-04-30 22:27:13 +0800

    fixes #512: geo download failed when startup
    - 启动阶段，executor还未初始化tunnel，tcpIn==nil导致geo下载失败,阻塞在
      tcpIn <- context

--- component/http/http.go
+++ component/http/http.go
@@ -53,8 +53,7 @@ func HttpRequest(ctx context.Context, url, method string, header map[string][]st
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 		DialContext: func(ctx context.Context, network, address string) (net.Conn, error) {
-			conn := inner.HandleTcp(address, "")
-			return conn, nil
+			return inner.HandleTcp(ctx, address)
 		},
 		TLSClientConfig: tls.GetDefaultTLSConfig(),
 	}
--- listener/inner/tcp.go
+++ listener/inner/tcp.go
@@ -1,9 +1,12 @@
 package inner
 
 import (
+	"context"
+	"net"
+
 	"github.com/Dreamacro/clash/adapter/inbound"
 	C "github.com/Dreamacro/clash/constant"
-	"net"
+	"github.com/Dreamacro/clash/log"
 )
 
 var tcpIn chan<- C.ConnContext
@@ -12,9 +15,16 @@ func New(in chan<- C.ConnContext) {
 	tcpIn = in
 }
 
-func HandleTcp(dst string, host string) net.Conn {
-	conn1, conn2 := net.Pipe()
-	context := inbound.NewInner(conn2, dst, host)
-	tcpIn <- context
-	return conn1
+func HandleTcp(ctx context.Context, address string) (conn net.Conn, err error) {
+	if tcpIn != nil {
+		// executor Parsed
+		conn1, conn2 := net.Pipe()
+		context := inbound.NewInner(conn2, address, "")
+		tcpIn <- context
+		return conn1, nil
+	}
+	log.Debugln("[Inner] executor not prepared, using direct dial, target=%s", address)
+	// At this point, no executor is available, so we need to
+	// dial the target server directly
+	return net.Dial("tcp", address)
 }
