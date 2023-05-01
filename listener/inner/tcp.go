463da578ddae85c6b6c03657377daad490286550
sleshep <9625413+sleshep@users.noreply.github.com>
2023-05-01 21:27:55 +0800

    fixes #512: geo download failed when startup (#538)
    * fixes #512: geo download failed when startup
    
    - 启动阶段，executor还未初始化tunnel，tcpIn==nil导致geo下载失败,阻塞在
      tcpIn <- context
    
    * chore: handled by the upper layer
    
    * chore: remove useless parameters
    
    ---------
    
    Co-authored-by: Skyxim <noreply@skyxim.dev>

--- listener/inner/tcp.go
+++ listener/inner/tcp.go
@@ -1,9 +1,11 @@
 package inner
 
 import (
+	"errors"
+	"net"
+
 	"github.com/Dreamacro/clash/adapter/inbound"
 	C "github.com/Dreamacro/clash/constant"
-	"net"
 )
 
 var tcpIn chan<- C.ConnContext
@@ -12,9 +14,13 @@ func New(in chan<- C.ConnContext) {
 	tcpIn = in
 }
 
-func HandleTcp(dst string, host string) net.Conn {
+func HandleTcp(address string) (conn net.Conn, err error) {
+	if tcpIn == nil {
+		return nil, errors.New("tcp uninitialized")
+	}
+	// executor Parsed
 	conn1, conn2 := net.Pipe()
-	context := inbound.NewInner(conn2, dst, host)
+	context := inbound.NewInner(conn2, address)
 	tcpIn <- context
-	return conn1
+	return conn1, nil
 }
