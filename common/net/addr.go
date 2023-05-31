4547664ab1e9078d13494c3948b5bdf6c2e113af
wwqgtxx <wwqgtxx@gmail.com>
2023-05-31 16:34:01 +0800

    Update addr.go

--- common/net/addr.go
+++ common/net/addr.go
@@ -2,10 +2,6 @@ package net
 
 import (
 	"net"
-
-	"github.com/Dreamacro/clash/log"
-
-	M "github.com/sagernet/sing/common/metadata"
 )
 
 type CustomAddr interface {
@@ -36,18 +32,5 @@ func NewCustomAddr(networkStr string, addrStr string, rawAddr net.Addr) CustomAd
 		networkStr: networkStr,
 		addrStr:    addrStr,
 		rawAddr:    rawAddr,
-	}
-}
-
-func CopyUDPAddr(netAddr net.Addr) *net.UDPAddr {
-	switch addr := netAddr.(type) {
-	case M.Socksaddr:
-		return addr.UDPAddr()
-	case *net.UDPAddr:
-		_addr := *addr
-		return &_addr // make a copy
-	default:
-		log.Fatalln("Unknown UDP address type: %T", addr)
-		return nil // unreachable
 	}
 }
