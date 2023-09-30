828b5ad8bb8b33cc8fc256e2a7e29d350570840e
wwqgtxx <wwqgtxx@gmail.com>
2023-09-30 23:55:56 +0800

    chore: add new bbr implementation

--- transport/tuic/common/congestion.go
+++ transport/tuic/common/congestion.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"github.com/Dreamacro/clash/transport/tuic/congestion"
+	congestionv2 "github.com/Dreamacro/clash/transport/tuic/congestion_v2"
 
 	"github.com/metacubex/quic-go"
 	c "github.com/metacubex/quic-go/congestion"
@@ -17,29 +18,23 @@ func SetCongestionController(quicConn quic.Connection, cc string, cwnd int) {
 		cwnd = 32
 	}
 	switch cc {
-	case "cubic":
-		quicConn.SetCongestionControl(
-			congestion.NewCubicSender(
-				congestion.DefaultClock{},
-				congestion.GetInitialPacketSize(quicConn.RemoteAddr()),
-				false,
-			),
-		)
-	case "new_reno":
-		quicConn.SetCongestionControl(
-			congestion.NewCubicSender(
-				congestion.DefaultClock{},
-				congestion.GetInitialPacketSize(quicConn.RemoteAddr()),
-				true,
-			),
-		)
-	case "bbr":
+	case "bbr_meta_v1":
 		quicConn.SetCongestionControl(
 			congestion.NewBBRSender(
 				congestion.DefaultClock{},
 				congestion.GetInitialPacketSize(quicConn.RemoteAddr()),
 				c.ByteCount(cwnd)*congestion.InitialMaxDatagramSize,
 				congestion.DefaultBBRMaxCongestionWindow*congestion.InitialMaxDatagramSize,
+			),
+		)
+	case "bbr_meta_v2":
+		fallthrough
+	case "bbr":
+		quicConn.SetCongestionControl(
+			congestionv2.NewBbrSender(
+				congestionv2.DefaultClock{},
+				congestionv2.GetInitialPacketSize(quicConn.RemoteAddr()),
+				c.ByteCount(cwnd),
 			),
 		)
 	}
