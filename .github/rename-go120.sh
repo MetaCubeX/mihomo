81a8a63861c56f1b81f59bc6b53d62d556a06baa
wwqgtxx <wwqgtxx@gmail.com>
2023-10-26 11:39:54 +0800

    build: more go120 build

--- .github/rename-go120.sh
+++ .github/rename-go120.sh
@@ -4,7 +4,7 @@ FILENAMES=$(ls)
 for FILENAME in $FILENAMES
 do
     if [[ ! ($FILENAME =~ ".exe" || $FILENAME =~ ".sh")]];then
-        mc $FILENAME ${FILENAME}-go120
+        mv $FILENAME ${FILENAME}-go120
     elif [[ $FILENAME =~ ".exe" ]];then
         mv $FILENAME ${FILENAME%.*}-go120.exe
     else echo "skip $FILENAME"
