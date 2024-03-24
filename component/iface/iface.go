79c9db97a95fcc499d757c6802a583e8dea40841
bobo liu <liu@bobo.moe>
2024-03-24 21:35:43 +0800

    chore: stylish d2ae94f2

--- component/iface/iface.go
+++ component/iface/iface.go
@@ -41,9 +41,8 @@ func ResolveInterface(name string) (*Interface, error) {
 			ipNets := make([]netip.Prefix, 0, len(addrs))
 			for _, addr := range addrs {
 				var pf netip.Prefix
-				switch addr.(type) {
+				switch ipNet := addr.(type) {
 				case *net.IPNet:
-					ipNet := addr.(*net.IPNet)
 					ip, _ := netip.AddrFromSlice(ipNet.IP)
 					ones, bits := ipNet.Mask.Size()
 					if bits == 32 {
@@ -51,7 +50,6 @@ func ResolveInterface(name string) (*Interface, error) {
 					}
 					pf = netip.PrefixFrom(ip, ones)
 				case *net.IPAddr:
-					ipNet := addr.(*net.IPAddr)
 					ip, _ := netip.AddrFromSlice(ipNet.IP)
 					ip = ip.Unmap()
 					pf = netip.PrefixFrom(ip, ip.BitLen())
