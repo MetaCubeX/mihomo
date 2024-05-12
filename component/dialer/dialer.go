3ae4014b393ea1dccb04f1f93c389cb1ab65b091
wwqgtxx <wwqgtxx@gmail.com>
2024-05-12 20:44:12 +0800

    chore: disable tfo when lower than Windows 10.0.14393

--- component/dialer/dialer.go
+++ component/dialer/dialer.go
@@ -164,7 +164,7 @@ func dialContext(ctx context.Context, network string, destination netip.Addr, po
 	if opt.mpTcp {
 		setMultiPathTCP(dialer)
 	}
-	if opt.tfo {
+	if opt.tfo && !DisableTFO {
 		return dialTFO(ctx, *dialer, network, address)
 	}
 	return dialer.DialContext(ctx, network, address)
