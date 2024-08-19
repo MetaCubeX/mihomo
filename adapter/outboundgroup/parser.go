c8380335cbdea02965876b443c070ff8cd048956
xishang0128 <xishang02@gmail.com>
2024-08-19 16:02:43 +0800

    chore: improve `include-all-proxies` compatibility

--- adapter/outboundgroup/parser.go
+++ adapter/outboundgroup/parser.go
@@ -88,6 +88,9 @@ func ParseProxyGroup(config map[string]any, proxyMap map[string]C.Proxy, provide
 		} else {
 			groupOption.Proxies = append(groupOption.Proxies, AllProxies...)
 		}
+		if len(groupOption.Proxies) == 0 && len(groupOption.Use) == 0 {
+			groupOption.Proxies = []string{"COMPATIBLE"}
+		}
 	}
 
 	if len(groupOption.Proxies) == 0 && len(groupOption.Use) == 0 {
