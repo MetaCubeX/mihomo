936113c26a9cf393d4ab70c688def5ef11d3d484
wwqgtxx <wwqgtxx@gmail.com>
2025-11-23 19:09:29 +0800

    Refactor proxy authorization parsing logic
    Refactor parseBasicProxyAuthorization to use a constant for the prefix and improve case-insensitivity handling.

--- listener/http/utils.go
+++ listener/http/utils.go
@@ -63,7 +63,11 @@ func removeExtraHTTPHostPort(req *http.Request) {
 // parseBasicProxyAuthorization parse header Proxy-Authorization and return base64-encoded credential
 func parseBasicProxyAuthorization(request *http.Request) string {
 	value := request.Header.Get("Proxy-Authorization")
-	if !(len(value) >= 6 && strings.EqualFold(value[:6], "Basic ")) {
+	const prefix = "Basic "
+	// According to RFC7617, the scheme should be case-insensitive.
+	// In practice, some implementations do use different case styles, causing authentication to fail
+	// eg: https://github.com/algesten/ureq/blob/381fd42cfcb80a5eb709d64860aa0ae726f17b8e/src/unversioned/transport/connect.rs#L118
+	if len(value) < len(prefix) || !strings.EqualFold(value[:len(prefix)], prefix) {
 		return ""
 	}
 
