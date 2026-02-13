83210cfa6911a932e567c953768a57260ec4e525
Panda <you@example.com>
2026-02-13 11:13:57 +0300

    fix: make User-Agent check case-insensitive

--- component/http/http.go
+++ component/http/http.go
@@ -50,7 +50,7 @@ func HttpRequest(ctx context.Context, url, method string, header map[string][]st
 		}
 	}
 
-	if _, ok := header["User-Agent"]; !ok {
+	if _, ok := req.Header["User-Agent"]; !ok {
 		req.Header.Set("User-Agent", UA())
 	}
 
