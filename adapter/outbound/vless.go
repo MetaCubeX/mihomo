64ae0875347f92905c2b1504af360d0096e52750
Nemu-x <fent95@gmail.com>
2026-03-30 00:10:22 +0300

    cleanup(vless): remove unused sc-max-concurrent-posts option

--- adapter/outbound/vless.go
+++ adapter/outbound/vless.go
@@ -76,7 +76,6 @@ type XHTTPOptions struct {
 	Host                 string            `proxy:"host,omitempty"`
 	Mode                 string            `proxy:"mode,omitempty"`
 	Headers              map[string]string `proxy:"headers,omitempty"`
-	ScMaxConcurrentPosts int               `proxy:"sc-max-concurrent-posts,omitempty"`
 	ScMaxEachPostBytes   int               `proxy:"sc-max-each-post-bytes,omitempty"`
 	ScMinPostsIntervalMs int               `proxy:"sc-min-posts-interval-ms,omitempty"`
 	NoGRPCHeader         bool              `proxy:"no-grpc-header,omitempty"`
