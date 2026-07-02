d2ce724e817d1eebe0d02c9d148b3b1e1380a958
rumianoesa <118131425+rumianoesa@users.noreply.github.com>
2026-07-03 04:35:34 +0800

    Fix realm resolver

--- listener/sing_hysteria2/server.go
+++ listener/sing_hysteria2/server.go
@@ -191,7 +191,7 @@ func New(config LC.Hysteria2Server, tunnel C.Tunnel, additions ...inbound.Additi
 				if ipv4 && !ipv6 {
 					return resolver.LookupIPv4WithResolver(ctx, host, resolver.ProxyServerHostResolver)
 				} else if ipv6 && !ipv4 {
-					return resolver.LookupIPv4WithResolver(ctx, host, resolver.ProxyServerHostResolver)
+					return resolver.LookupIPv6WithResolver(ctx, host, resolver.ProxyServerHostResolver)
 				}
 				return resolver.LookupIPWithResolver(ctx, host, resolver.ProxyServerHostResolver)
 			},
