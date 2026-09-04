dbc1fd6166e71d7b1d0b6b44998a82ca4f256e9d
edwardzh <i@edzh.me>
2026-09-04 11:00:17 +0800

    chore(ebpf): make BPF generation portable

--- listener/ebpf/generate.go
+++ listener/ebpf/generate.go
@@ -1,3 +1,3 @@
 package ebpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc /opt/homebrew/Cellar/llvm/23.1.0/bin/clang -strip /opt/homebrew/Cellar/llvm/23.1.0/bin/llvm-strip -cflags "-O2 -g -target bpfel" datapath ./bpf/datapath.c -- -I./bpf -I./bpf/include
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -strip llvm-strip -cflags "-O2 -g -target bpfel" datapath ./bpf/datapath.c -- -I./bpf -I./bpf/include
